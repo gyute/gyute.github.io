cd8ccfdacad9630be3629534f069a5d6f6e5702a
gyute <9tempo@gmail.com>
2024-09-29 21:00:28 +0900

    Start using google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "gyute.github.io",
@@ -13,6 +14,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=G-QVPPDWL1LQ`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-QVPPDWL1LQ');
+          `,
+          }}
+        />
+      </Head>
       <body>{children}</body>
     </html>
   );
