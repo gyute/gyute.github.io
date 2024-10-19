556dfdef09bbd264da7491f0cafff82b7d859c44
gyute <9tempo@gmail.com>
2024-10-19 23:53:58 +0900

    Minor style changes

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -31,15 +31,16 @@ export default async function Post({ params }: PostProps) {
   return (
     <>
       <div className="container">
-        <h2 className="text-4xl dark-text light-text mb-5">{post.title}</h2>
+        <h1 className="text-4xl dark-text light-text mb-5">{post.title}</h1>
         <p className="text-gray-500 text-lg mb-12">{postDate(post.date)}</p>
         <div
           className="
             text-lg dark-text light-text my-3
             [&_p]:my-3
-            [&_h1]:text-5xl [&_h1]:my-7
-            [&_h2]:text-4xl [&_h2]:my-6
-            [&_h3]:text-3xl [&_h3]:my-5
+            [&_h1]:text-4xl [&_h1]:my-7
+            [&_h2]:text-3xl [&_h2]:my-6
+            [&_h3]:text-2xl [&_h3]:my-5
+            [&_h4]:text-xl [&_h4]:my-4
             [&_img]:mx-auto
             [&_pre]:rounded-lg
             [&_ul]:pl-5 [&_li]:my-1
