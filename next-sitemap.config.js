768016382cf3d1e80657db5af8713b7e2d3ffbb2
gyute <9tempo@gmail.com>
2024-11-26 23:17:58 +0900

    Add next-sitemap config

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: "https://gyute.com",
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  autoLastmod: false,
+};
+
+module.exports = config;
