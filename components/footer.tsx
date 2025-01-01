61da510c4f3193a43f830263da32971ae2bad524
gyute <9tempo@gmail.com>
2025-01-02 00:00:07 +0900

    Update copyright year

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,7 +6,7 @@ export const Footer: React.FC = () => {
   return (
     <footer className="pb-4 pt-20">
       <div className="container mx-auto flex justify-between text-center">
-        <h3>Copyright &copy; 2024 Gyute</h3>
+        <h3>Copyright &copy; 2024-2025 Gyute</h3>
         <div className="flex items-center justify-center gap-4">
           <Link href="https://github.com/gyute">
             <AiOutlineGithub className="light-text dark-text hover-color size-8 active:animate-spin" />
